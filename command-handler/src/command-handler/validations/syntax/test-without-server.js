d1dd570744d9b176b69cec21795eda049559eeff
Adam <wild@lerndmina.dev>
2022-09-18 17:06:17 +0100

    Pretty message

--- command-handler/src/command-handler/validations/syntax/test-without-server.js
+++ command-handler/src/command-handler/validations/syntax/test-without-server.js
@@ -5,5 +5,5 @@ module.exports = (command) => {
         return
     }
     // testOnly is true and there are no test servers provided, so we throw an error
-    throw new Error(`The command ${commandName} is set to testOnly, but no test servers were provided.`)
+    throw new Error(`=-=-=-=-=-=-=\nThe command "${commandName}" is set to testOnly, but no test servers were provided.\nPlease provide test servers "./bot/src/index.js" or set testOnly to false.\n=-=-=-=-=-=-=`)
 }
