b31eeafe90d9c8971523a12e49ab76d45eb5ccd6
Adam <wild@lerndmina.dev>
2022-09-18 17:06:07 +0100

    Oops, forgot the _

--- command-handler/src/command-handler/Command.js
+++ command-handler/src/command-handler/Command.js
@@ -1,21 +1,21 @@
-class Command{
-    constructor(instance, commandName, commandObject){
-        this._instance = instance
-        this._commandName = commandName.toLowerCase()
-        this._commandObject = commandObject
-    }
+class Command {
+  constructor(instance, commandName, commandObject) {
+    this._instance = instance
+    this._commandName = commandName.toLowerCase()
+    this._commandObject = commandObject
+  }
 
-    get instance(){
-        return this.instance
-    }
+  get instance() {
+    return this._instance
+  }
 
-    get commandName(){
-        return this._commandName
-    }
+  get commandName() {
+    return this._commandName
+  }
 
-    get commandObject(){
-        return this._commandObject
-    }
+  get commandObject() {
+    return this._commandObject
+  }
 }
 
 module.exports = Command
